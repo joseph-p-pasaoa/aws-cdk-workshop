49b0c33db3e440f7815dd5b9f2b3da0667670872
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2021-05-14 00:04:31 -0400

    add api gateway to deployment

--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class AwsCdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -11,5 +12,11 @@ export class AwsCdkWorkshopStack extends cdk.Stack {
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
       handler: 'hello.handler'                // file is "hello", fx is "handler"
     });
+
+    // define API Gateway REST API resource backing "hello" fx
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
+
   }
 }
