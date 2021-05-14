6d2ef1bf132c8787bfc4133ca9c47c353dbd209a
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2021-05-13 23:48:20 -0400

    create Hello.Handler lambda fx

--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -1,7 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class AwsCdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // define AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", fx is "handler"
+    });
   }
 }
